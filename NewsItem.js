27863afa3e2499b9439b8f1ba30beffb91fe1bfb
Priyanka3223 <118335919+Priyanka3223@users.noreply.github.com>
2023-07-09 15:04:02 +0530

    Add files via upload

---
+++ NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  
+  render() {
+    let {title, description,imageUrl,newsUrl,author,date}=this.props;
+    return (
+      <div className='my-3'>
+        <div className="card" style={{width: "18rem"}}>
+        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text my-1"><small className="text-muted">By {author?author:"unknown"} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_black" className="btn btn-sm btn-dark">Read More</a>
+        </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
